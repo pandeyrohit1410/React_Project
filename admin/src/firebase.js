79126f810b3f25c9b1683309bbf8111eeb995eb3
pandeyrohit1410 <pandeyrohit1410@gmail.com>
2022-12-27 12:39:45 +0530

    first commit

---
+++ admin/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyByEAT3FSZB8cU75RkKrfcsYMxfN5p4N8E",
+  authDomain: "indiamart-762a8.firebaseapp.com",
+  projectId: "indiamart-762a8",
+  storageBucket: "indiamart-762a8.appspot.com",
+  messagingSenderId: "394405786117",
+  appId: "1:394405786117:web:62e203c8f4be4403d1626c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
